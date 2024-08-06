4120c3d4968017ecb55248e24f4a61456ceb3c62
Rakesh709 <kumar.rakesh1997@outlook.com>
2024-08-06 23:29:21 +0530

    Number Guess

---
+++ NumberGuess/index.js
@@ -0,0 +1,62 @@
+
+const randomNumber = parseInt(Math.random()*100+1)
+
+const submit = document.querySelector('#subt');
+const userInput = document.querySelector("#guessField")
+const guessSlot = document.querySelector('.guesses')
+const remaing = document.querySelector('.lastResult')
+const lowOrHigh = document.querySelector('.lowOrHi')
+const startOver = document.querySelector('.resultParas')
+
+const p = document.createElement('p')
+
+let prevGuess = [];
+let numGuess = 1;
+
+let playGame = true;
+
+if(playGame){
+    submit.addEventListener('click',function(e){
+        e.preventDefault();
+        const guess= parseInt(userInput.value)
+        console.log(guess)
+        validateGuess(guess)
+    })
+}
+
+
+function validateGuess(guess){
+    //to validate valid number
+    if(isNaN(guess)){
+        alert("Please enter a valid number")
+    }else if(guess<1){
+        alert("Please enter a number greater than 1")
+    }else if(guess>100){
+        alert("Please enter a number less than 100")
+    }else{
+        
+    }
+}
+
+
+function checkGuess(guess){
+    //
+}
+
+function displayGuess(guess){
+
+}
+
+function displayMessage(message){
+
+}
+
+
+function endGame(){
+
+}
+
+
+function newGame(){
+
+}
