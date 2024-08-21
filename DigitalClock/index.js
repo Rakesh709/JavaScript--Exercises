04a20d53caddef4b51d84f6916c2b7a8e0462853
Rakesh709 <kumar.rakesh1997@outlook.com>
2024-08-22 00:28:51 +0530

    added digital clock

--- DigitalClock/index.js
+++ DigitalClock/index.js
@@ -4,3 +4,19 @@ setInterval(function(){
     let date = new Date();
     clock.innerHTML = date.toLocaleTimeString()
 },1000)
+
+
+const intClock = document.getElementById("int-clock")
+
+setInterval(function(){
+    let date = new Date();
+    intClock.innerHTML= date.toLocaleTimeString("en-US", {timeZone: "America/New_York"});
+},1000)
+
+
+const clock3= document.querySelector("#london-clock")
+
+setInterval(function(){
+    let date= new Date();
+    clock3.innerHTML = date.toLocaleTimeString("en-US", {timeZone: "Europe/London"})
+},1000)
