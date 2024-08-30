84b07fe14269996040da51aed22e37dc7fe7584d
Rakesh709 <kumar.rakesh1997@outlook.com>
2024-08-30 23:45:43 +0530

    array

---
+++ iteration/array1.js
@@ -0,0 +1,28 @@
+for(let i=0; i<=10; i++){
+    const elem= i
+    //console.log(`elem is ${elem}`);
+    
+}
+
+for(let i=0; i<10;i++){
+    //console.log(`outer loop value is ${i}`);
+    for(let j=0; j<10;j++){
+        //console.log(`innter loop value is ${j} and inner is ${i}`);    
+    }
+}
+
+
+for(let i=1;i<=10;i++){
+    //console.log(i);
+    for(let j=1;j<=10;j++){
+        //console.log(i ,"*",j,"=",i*j)
+    }
+    
+}
+
+let myArray = ["luffy","naruto","deku","saitama","goku"]
+
+for( let i=0; i<myArray.length; i++){
+    console.log(myArray[i]);
+    
+}
