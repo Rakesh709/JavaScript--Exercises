acb4992e51e26567dcff1b19eea12478a954edd9
Rakesh709 <kumar.rakesh1997@outlook.com>
2024-08-31 20:18:16 +0530

    array

--- iteration/array1.js
+++ iteration/array1.js
@@ -23,6 +23,34 @@ for(let i=1;i<=10;i++){
 let myArray = ["luffy","naruto","deku","saitama","goku"]
 
 for( let i=0; i<myArray.length; i++){
-    console.log(myArray[i]);
+    //console.log(myArray[i]);
     
 }
+
+// #1 Print the numbers from 0 to 15
+
+for(let i=0; i<=15; i++){
+    //console.log(i);
+    
+}
+
+//#2 — Print the numbers from 12 to 24
+
+for(let i=12; i<=24; i++){
+    //console.log(i);
+    
+}
+
+//#3 — Print the ODD numbers from 7 to 31
+
+for(let i=7; i<=31; i++){
+    if(i%2!=0){
+        //console.log(`this is even ${i}`);
+        
+    }
+}
+
+// #5 — Iterate through all numbers from 1 to 
+// For multiples of 3 print “Fizz”
+// For multiples of 5 print “Buzz”
+// For multiples of 3 and 5 print “FizzBuzz”
