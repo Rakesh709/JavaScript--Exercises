df888854703979f12d2d301f5e50e0b980062f8e
Rakesh709 <kumar.rakesh1997@outlook.com>
2024-10-02 00:00:46 +0530

    constructor function

---
+++ classes_oop/constructorFunction.js
@@ -0,0 +1,63 @@
+// function greet(name){
+//     console.log("hello", name);   
+// }
+
+//best approach
+
+function Person(name,age){
+    this.name= name
+    this.age= age
+}
+
+function Car(make,model){
+    this.make= make
+    this.model= model
+}
+
+let myCar = new Car("Toyota","Camry")
+//console.log(myCar);
+
+
+let myNewCar= new Car("Tata","Safari")
+//console.log(myNewCar);
+
+
+function Tea(type){
+   this.type= type
+   this.describe= function(){
+    return `this is a cup of ${this.type}`
+   }
+}
+
+let lemonTea = new Tea("Lemon Tea")
+//console.log(lemonTea.describe());
+
+
+function Animal(species){
+    this.species= species
+}
+
+Animal.prototype.sound= function(){
+    return `${this.species} makes a sound`
+}
+
+let dog = new Animal("Dog")
+// console.log(dog);
+// console.log(dog.sound());
+
+
+let cat = new Animal("Cat")
+// console.log(cat);
+// console.log(cat.sound());
+
+//error handling
+
+function Drink(name){
+    if(!new.target){
+        throw new Error("Drink must be called with new Keyword")
+    }
+    this.name = name;
+}
+
+let tea = new Drink("Tea")
+let coffee = Drink("Coffee")
