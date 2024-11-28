2f32b2bd31e95e1f683627ef76d6e4918365cae3
Rakesh709 <kumar.rakesh1997@outlook.com>
2024-11-29 01:32:26 +0530

    interview

--- Notepad/index3.js
+++ Notepad/index3.js
@@ -43,6 +43,49 @@ function findLargestPair(array){
     }
 }
 
-const result = findLargestPair([10, 5, 20, 15, 25, 2]);
-console.log("Largest Pair:", result.largestPair);
-console.log("Indices:", result.indices);
+// const result = findLargestPair([10, 5, 20, 15, 25, 2]);
+// console.log("Largest Pair:", result.largestPair);
+// console.log("Indices:", result.indices);
+
+
+//Code 21: To find the largest pair of the 2 elements using indexing with unsorted elements 
+function sortedArray(arr){
+    let sortArr = arr.sort((a,b) => b-a)
+    
+    console.log(sortArr);
+    
+    return sortArr[0]+sortArr[1]
+     
+}
+
+//console.log(sortedArray([4,6,2,8,9]));
+ 
+
+//Code 22: To find the largest pair of the 2 elements using indexing with sorted elements
+
+function sortElement(arr){
+    let sortArr = arr.sort((a,b)=>b-a)
+    console.log(sortArr);
+    console.log(arr);
+    
+    return sortArr[0]+sortArr[1]
+}
+
+//console.log(sortElement([4,6,2,8,9]));
+
+//Code 23: To find the index of an element from an array 
+
+let arr = ["a","b","c"]
+
+let indexArr = arr.indexOf("c")
+
+//console.log(indexArr);
+
+
+//Code 24: Fibonacci Series (0,1,1,2,3,5,8,13....)
+
+function fibSeries(n){
+    return n * fibSeries(n-1)
+}
+
+console.log(fibSeries(3));
