3c392fabd6661ec4bb3c00630bd1749ca32af9f7
Rakesh709 <kumar.rakesh1997@outlook.com>
2025-01-01 20:53:51 +0530

    pattern coding

--- Notepad/05pattern.js
+++ Notepad/05pattern.js
@@ -1,9 +1,61 @@
 //Triangle Pattern — I
+// 1
+// 1 2
+// 1 2 3
+// 1 2 3 4
+// 1 2 3 4 5
 
-for (let i = 1; i <= 5; i++) {
-  let row = "";
-  for (let j = 1; j <= i; j++) {
-    row += j + " ";
-  }
-  console.log(row.trim());
+// for(let i=1;i<=5;i++){
+//     let num='';
+//     for(let j=1; j<=i; j++){
+//         num+=j
+//     }
+//     console.log(num);
+    
+// }
+
+
+// Triangle Pattern — II
+// 1
+// 2 2
+// 3 3 3
+// 4 4 4 4
+// 5 5 5 5 5
+
+// for(let i=1; i<=5; i++){
+//     let row='';
+//     for(let j=1; j<=i; j++){
+//         row=j+" "  
+//     }
+//     console.log(row.trim());   
+// }
+
+// for(let i=1; i<=5;i++){
+//     let num='';
+//     for(let j=1; j<=i ;j++){
+//         num+=i
+//     }
+//     console.log(num);
+    
+// }
+
+//Triangle Pattern — III
+
+// 1
+// 2 3
+// 4 5 6
+// 7 8 9 10
+
+let n=4;
+
+let num=1;
+
+for(let i=1; i<=n;i++){
+    let row='';
+    for(let j=1; j<=i;j++){
+        row+=num+''
+        num++
+    }
+    console.log(row.trim());
+    
 }
