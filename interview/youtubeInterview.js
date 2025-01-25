ef1b03d74327af342f400becb30a55e0cce52aa1
Rakesh709 <kumar.rakesh1997@outlook.com>
2025-01-26 01:29:07 +0530

    common finder

--- interview/youtubeInterview.js
+++ interview/youtubeInterview.js
@@ -51,9 +51,18 @@
 let arr1=[1,2,3,4]
 let arr2=[2,4,6]
 
-let arr=[...arr1,...arr2]
-console.log(arr);
+// let comArray = arr1.filter(element=>arr2.includes(element))
+// console.log(comArray);
 
+let comArray =[]
 
+for(let i=0; i<arr1.length; i++){
+  for(let j=0; j<arr2.length; j++){
+    if(arr1[i]===arr2[j]){
+      comArray.push(arr1[i])
+    }
+  }
+}
 
+console.log(comArray);
 
