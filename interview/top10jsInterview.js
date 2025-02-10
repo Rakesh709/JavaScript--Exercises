e6cb1a01686eb60db621717a1545936cc64ad290
Rakesh709 <kumar.rakesh1997@outlook.com>
2025-02-10 08:44:44 +0530

    interview

--- interview/top10jsInterview.js
+++ interview/top10jsInterview.js
@@ -1,20 +1,40 @@
-https://www.youtube.com/watch?v=XjUR_3dNAJ0&t=37s
+//https://www.youtube.com/watch?v=XjUR_3dNAJ0&t=37s
 
 
 //1.what is event loop?
+let one = () =>{
+    console.log("I AM ONE");
+    
+};
+one();
+// let two = ()=>{
+//     setInterval(()=>{
+//         console.log("Whooooooo");
+//     },360)
+//     console.log("I am Two");  
+// }
+
+// let three = ()=>{
+//     console.log("I am Three");  
+// }
+
+
+// two();
+// three();
+
 
 
 //2.diffrence between map and forEach
-let arr1=[1,2,3];
-let mulArray= arr1.map((num)=>num*2)
+// let arr1=[1,2,3];
+// let mulArray= arr1.map((num)=>num*2)
 
 //console.log(mulArray);
 
-let arr2=[1,2,3];
-let eachArray=[]
-arr2.forEach(num=>{
-    eachArray.push(num*2)
-})
+// let arr2=[1,2,3];
+// let eachArray=[]
+// arr2.forEach(num=>{
+//     eachArray.push(num*2)
+// })
 
 //console.log(eachArray);
 //agrer kuch operation krna hai to
