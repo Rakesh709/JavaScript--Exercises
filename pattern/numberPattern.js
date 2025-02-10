515715a6c611401c8bb4a34ae7b8e536da205ae8
Rakesh709 <kumar.rakesh1997@outlook.com>
2025-02-11 03:15:49 +0530

    pattern

--- pattern/numberPattern.js
+++ pattern/numberPattern.js
@@ -36,12 +36,76 @@ for(let i=1;i <=m; i++){
 // 4 5 6
 // 7 8 9 10
 
+let o=4;
 let count =1;
-for(let i=1; i<=4;i++){
+for(let i=1; i<=0;i++){
+    let row =""
     for(let j=1; j<=i; j++){
-        // console.log(count)
+        row+=count+" "
         count++
     }
-    console.log(count.trim());
+    //console.log(row.trim());
+    
+}
+
+//4. 
+// 12345
+// 1234
+// 123
+// 12
+// 1
+
+//5
+for(let row=1; row<=4; row++){
+    let start=""
+    for(let col=1; col<=row; col++){
+        // process.stdout.write("*")
+        start+="*"
+        
+    }
+    //console.log(start.trim());
+    
+}
+
+//6
+// ****
+// ****
+// ****
+// ****
+for(let row=1; row<=6; row++){
+    let start=""
+    for(let col=1; col<=6; col++){
+        // process.stdout.write("*")
+        start+="*"
+    }
+    //console.log(start)
+}
+
+//7
+// *****
+// ****
+// ***
+// **
+// *
+for(let row=1; row<=5; row++){
+    let start=""
+    for(let col=1; col<=5-row+1; col++){
+        start+="*"
+    }
+    //console.log(start);
+    
+}
+
+
+//8 
+
+let x=5
+for(let row=0; row<=(2*x); row++){
+    let star=""
+    let num= row >x ? 2*x -row : row;
+    for(let col=0; col<num; col++){
+         star+="*"
+    }
+    console.log(star);
     
 }
